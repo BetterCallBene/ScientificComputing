da04490e1343583b367808de8170189a3e0ba292
BetterCallBene <benedikt.koenig@tum.de>
2016-01-13 22:15:23 +0100

    add dyn_unittest.cu

---
+++ Project/tests/dyn_unittest.cu
@@ -0,0 +1,11 @@
+#define BOOST_TEST_MODULE MultiShooting Dynamic 
+#include <boost/test/included/unit_test.hpp>
+
+#include "dyn.hpp"
+int add(int i, int j){return i + j;}
+
+BOOST_AUTO_TEST_CASE(Constructor)
+{
+	BOOST_CHECK(add(2, 2) == 4);
+}
+
