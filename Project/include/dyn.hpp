973296faadca931aa69a5e01722f7fe39e8ff5bc
BetterCallBene <benedikt.koenig@tum.de>
2016-01-14 22:17:29 +0100

    dyn.hpp

--- Project/include/dyn.hpp
+++ Project/include/dyn.hpp
@@ -1,7 +1,11 @@
+#include <thrust/device_vector.h>
+#include <boost/numeric/odeint/external/thrust/thrust.hpp>
+
 #include "config.hpp"
-//#include "dyn_base.h"
+#include "sys_base_functor.hpp"
 
 #pragma once
+using namespace std;
 
 class dyn
 {
@@ -15,13 +19,23 @@ class dyn
 	value_type m_u[3];
 public:
 
-	struct sys_functor : public sys_base_functor
+	struct sys_functor : sys_base_functor
 	{
+		
+		sys_functor(value_type g, value_type m, value_type kT,
+			value_type kQ, value_type d, value_type IM, 
+			value_type* Iges, value_type *u) : sys_base_functor(g, m, kT, kQ, d, IM, Iges, u)
+		{
+			
+		}
 		template <class Tuple>
 		__host__ __device__ 
 		void operator()(Tuple t)
 		{
-			const value_type r[3] = {thrust::get<0>(t), thrust:get<1>(t), thrust:get<2>(t)}
+			const value_type r[3] = {thrust::get<0>(t), thrust::get<1>(t), thrust::get<2>(t)};
+			const value_type q[4] = {thrust::get<3>(t), thrust::get<4>(t), thrust::get<5>(t), thrust::get<6>(t)};
+			const value_type v[3] = {thrust::get<7>(t), thrust::get<8>(t), thrust::get<9>(t)};
+			const value_type omega[3] = {thrust::get<10>(t), thrust::get<11>(t), thrust::get<12>(t)};
 		}
 	};
 	dyn(value_type g, value_type m, value_type kT, 
