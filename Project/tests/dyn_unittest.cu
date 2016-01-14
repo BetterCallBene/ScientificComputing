9327d82df1ed618a1befe1a278854b712f3e2095
Benedikt König <Bene@Benedikts-MBP.fritz.box>
2016-01-14 21:32:12 +0100

    dyn test

--- Project/tests/dyn_unittest.cu
+++ Project/tests/dyn_unittest.cu
@@ -30,9 +30,9 @@ void getInitialValues(value_type* g, value_type* m, value_type* kT,
 	string strIges =  pt.get<string>("environment.Iges");
 	vector<string> strs;
         boost::split(strs,strIges,boost::is_any_of("(,"));
-	cout << string_to_double(strs[1]);
-	cout << string_to_double(strs[2]);
-	cout << string_to_double(strs[3]);
+	cout << (strs[1]);
+	cout << (strs[2]);
+	cout << (strs[3]);
 }
 
 BOOST_AUTO_TEST_CASE(Constructor)
